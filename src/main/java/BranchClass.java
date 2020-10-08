b342f8c295c5d9235b97e3c86d5ebf14fb298d3d
yusuftml <yusuftemel355@gmail.com>
2020-10-08 16:01:03 +0200

    second change

---
+++ src/main/java/BranchClass.java
@@ -0,0 +1,6 @@
+public class BranchClass {
+    public static void main(String[] args) {
+        System.out.println("Branch is a amazing feature");
+        //i am pushing my class to git
+    }
+}
---
+++ src/test/java/BranchTest.java
@@ -0,0 +1,5 @@
+public class BranchTest {
+    public static void main(String[] args) {
+        System.out.println("Branch test test test");
+    }
+}
